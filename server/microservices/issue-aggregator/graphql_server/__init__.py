c87b4f706c4da3808211775087f71d9cd7cda251
MissTipo <dorine.a.tipo@gmail.com>
2025-02-20 13:19:48 +0300

    chore(graphql): configure context getter for DB dependency injection
    - Add a context getter that injects the DB session (using Depends(get_db)) into the GraphQL context.
    - Pass the context_getter to the GraphQLRouter to ensure resolvers receive the database session.

--- server/microservices/issue-aggregator/graphql_server/__init__.py
+++ server/microservices/issue-aggregator/graphql_server/__init__.py
@@ -1,23 +1,31 @@
 from strawberry.fastapi import GraphQLRouter
-
 import strawberry
-from typing import List, Union
+from typing import List
 from .schemas.issue_schema import Issue
-from .resolvers.issue_resolver import QueryResolver, MutationResolver
+from .resolvers.issue_resolver import QueryResolver
+from models.database import get_db
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+# Define the context function
+
+def get_context(db: Session = Depends(get_db)):
+    return {"db": db}
 
 @strawberry.type
 class Query:
     issues: List[Issue] = strawberry.field(resolver=QueryResolver.get_issues)
-    issue: Issue = strawberry.field(resolver=QueryResolver.get_issue)
+    issue: Issue = strawberry.field(resolver=QueryResolver.get_issue_by_id)
+    issues_by_state: List[Issue] = strawberry.field(resolver=QueryResolver.get_issues_by_state)
+    issues_by_source: List[Issue] = strawberry.field(resolver=QueryResolver.get_issues_by_source)
+    issues_by_label: List[Issue] = strawberry.field(resolver=QueryResolver.get_issues_by_label)
 
-@strawberry.type
+"""@strawberry.type
 class Mutation:
     create_issue: Issue = strawberry.field(resolver=MutationResolver.create_issue)
     update_issue: Issue = strawberry.field(resolver=MutationResolver.update_issue)
-    delete_issue: Issue = strawberry.field(resolver=MutationResolver.delete_issue)
+    delete_issue: Issue = strawberry.field(resolver=MutationResolver.delete_issue)"""
 
-
-
-
-schema = strawberry.Schema(query=Query, mutation=Mutation)
-graphql_app = GraphQLRouter(schema)
+# Add the context to the schema
+schema = strawberry.Schema(query=Query)
+graphql_app = GraphQLRouter(schema, context_getter=get_context)
