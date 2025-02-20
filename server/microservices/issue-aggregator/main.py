47bc5e5915f69bc3af6e36ec660e18107c973e48
MissTipo <dorine.a.tipo@gmail.com>
2025-02-20 19:47:00 +0300

    chore(main): load environment variables
    - Use python-dotenv to load GITHUB_TOKEN, GITLAB_TOKEN, and DATABASE_URL from the .env file.

--- server/microservices/issue-aggregator/main.py
+++ server/microservices/issue-aggregator/main.py
@@ -1,3 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+GITLAB_TOKEN = os.environ.get("GITLAB_TOKEN")
+DATABASE_URL = os.getenv("POSTGRES_DB_URL", "sqlite:///./test.db")
+
 from fastapi import FastAPI
 from graphql_server import graphql_app
 
