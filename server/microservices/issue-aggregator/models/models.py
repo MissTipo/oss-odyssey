5e34bde787c486ff89f43b402734f20159388b76
MissTipo <dorine.a.tipo@gmail.com>
2025-02-20 13:18:06 +0300

    refactor(models): remove custom __init__ from Issues model
    - Remove the custom __init__ method from the Issues model to allow SQLAlchemy’s default initializer.
    - Ensure model fields are initialized via keyword arguments for ORM compatibility.

--- server/microservices/issue-aggregator/models/models.py
+++ server/microservices/issue-aggregator/models/models.py
@@ -2,18 +2,20 @@ from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
 
 class Issues(Base):
-    __tablename__ = 'issues'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(50))
-    description = Column(String(200))
-    state = Column(Boolean)
-    
-    source = Column(String(50))
+    """ Issues Model """
 
-    def __init__(self, title=None, description=None, status=None):
+    __tablename__ = 'issues'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50), nullable=False)
+    description = Column(String(200), nullable=True)
+    state = Column(Boolean, default= False)
+    source = Column(String(50), nullable=True)
+
+    """def __init__(self, title=None, description=None, status=None):
         self.title = title
         self.description = description
-        self.status = status
+        self.state = state
+        self.source = source"""
 
     def __repr__(self):
         return '<Issue %r>' % (self.title)
