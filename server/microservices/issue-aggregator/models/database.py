5deaa6d9bf3831f0f9304723c7b7289d7540d67d
MissTipo <dorine.a.tipo@gmail.com>
2025-02-26 12:30:05 +0300

    Improve database connection handling
    - Import NullPool from sqlalchemy.pool
    - Use NullPool and enable pool_pre_ping in engine creation
    - Set autocommit=False and expire_on_commit=False in sessionmaker

--- server/microservices/issue-aggregator/models/database.py
+++ server/microservices/issue-aggregator/models/database.py
@@ -3,15 +3,16 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 from sqlalchemy.orm import sessionmaker
 from decouple import config
 
 # Database connection
 SQLALCHEMY_DATABASE_URL = config("POSTGRES_DB_URL")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, poolclass=NullPool)
 
 # create session factory
-DBSession = sessionmaker(bind=engine, autoflush=False)
+DBSession = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
 
 # Dependency to get DB session
 def get_db():
