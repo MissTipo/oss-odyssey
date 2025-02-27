659a331cd891faf9b2ded068dde28587edb307d5
MissTipo <dorine.a.tipo@gmail.com>
2025-02-27 18:17:19 +0300

    fix(models): update declarative_base import
    - Replace sqlalchemy.ext.declarative.declarative_base with sqlalchemy.orm.declarative_base to avoid deprecation warnings.

--- server/microservices/user-management-and-authentication/models/database.py
+++ server/microservices/user-management-and-authentication/models/database.py
@@ -1,13 +1,14 @@
+from sqlalchemy.orm import declarative_base
+Base = declarative_base()
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = os.environ.get("POSTGRES_DB_URL", "sqlite:///./test.db")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
