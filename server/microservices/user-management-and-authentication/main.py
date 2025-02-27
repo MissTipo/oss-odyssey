6e0bc70a1ac368bb90bdbbb3835777d40ec6210a
MissTipo <dorine.a.tipo@gmail.com>
2025-02-27 18:14:38 +0300

    feat(main): add authMiddleware registration
    - Register authMiddleware in the FastAPI app to handle authentication on incoming requests.

--- server/microservices/user-management-and-authentication/main.py
+++ server/microservices/user-management-and-authentication/main.py
@@ -8,8 +8,11 @@ DATABASE_URL = os.getenv("POSTGRES_DB_URL", "sqlite:///./test.db")
 
 from fastapi import FastAPI
 from graphql_server import graphql_app
+from middleware.auth import authMiddleware
 
 app = FastAPI()
 
+# Add the `authMiddleware` to the list of middleware
+app.middleware(authMiddleware)
 # Add the `/graphql` route and set the `graphql_app` as its route handler
 app.include_router(graphql_app, prefix="/graphql")
