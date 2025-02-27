14725e1c1f3d113fb89801354785837a35d296f2
MissTipo <dorine.a.tipo@gmail.com>
2025-02-27 18:16:30 +0300

    feat(user): add all_users query and user mutations
    - Update ORMUser import path to models.models.
    - Import datetime and List for additional type support.
    - Add all_users resolver to fetch and return a list of users.
    - Introduce UserMutationResolver with updateUser and deleteUser mutations.

--- server/microservices/user-management-and-authentication/graphql_server/schemas/user_schema.py
+++ server/microservices/user-management-and-authentication/graphql_server/schemas/user_schema.py
@@ -1,5 +1,6 @@
 import strawberry
 from datetime import datetime
+from typing import Optional
 
 @strawberry.type
 class User:
@@ -9,4 +10,12 @@ class User:
     is_active: bool
     created_at: datetime
     updated_at: datetime
+
+@strawberry.input
+class UpdateUserInput:
+    id: int
+    username: Optional[str] = None
+    email: Optional[str] = None
+    is_active: Optional[bool] = None
+
 
